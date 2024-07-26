e19a937201df08cbae9d482e504077dacf448f0f
Ruchi <ruchithakor23@gmail.com>
2024-07-25 22:10:11 -0400

    test policy granular

--- code/granular_level_implementation/test_policy_granular.py
+++ code/granular_level_implementation/test_policy_granular.py
@@ -6,12 +6,11 @@ import numpy as np
 import pandas as pd
 
 # Load the required data files
-all_women_data = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/dataset/all_women_data.csv")
 rewards_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/dataset/rewards.csv")
-transitions_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/dataset/state_transition_probabilities.csv")
+transitions_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/dataset/state_transition_probabilities_granular.csv")
 
 # Define the set of states, actions, rewards, and transition probabilities
-S = np.sort(all_women_data["Risk Score"].unique())
+S = np.round(np.arange(0, 100.1, 0.1), 1)
 A = ["AM", "B"]  # Actions: Annual Mammogram (AM) and Biopsy (B)
 T = 60  # Time steps, e.g., from age 40 to 100
 
@@ -20,14 +19,14 @@ R = {row["age"]: {
         "Healthy": {"AM": row["Healthy_AM"], "B": row["Healthy_B"]},
         "Early": {"AM": row["Early_AM"], "B": row["Early_B"]},
         "Advanced": {"AM": row["Advanced_AM"], "B": row["Advanced_b"]}
-    } for index, row in rewards_df.iterrows()}
+    } for _, row in rewards_df.iterrows()}
 
 # Initialize the transition probabilities dictionary for each age
 P = {age: {} for age in range(40, 100)}
 
 # Populate the dictionary with data from the DataFrame
 for ind, row in transitions_df.iterrows():
-    
+
     age = int(row["Age"])
     from_state = int(row["From State"])
     to_state = int(row["To State"])
@@ -52,16 +51,16 @@ for age in range(40, 100):
 
 # Function to initialize the occupancy distribution
 def initialize_occupancy_distribution(max_prob_state_1, max_prob_state_2):
-    
+
     prob_state_1 = np.random.uniform(0, max_prob_state_1) # probability of early stage cancer
     prob_state_2 = np.random.uniform(0, max_prob_state_2) # probability of advanced stage cancer
-
+    
     if prob_state_1 + prob_state_2 > 1:
         prob_state_2 = np.random.uniform(0, 1 - prob_state_1)
     
     prob_state_0 = 1.0 - prob_state_1 - prob_state_2 # probability of being healthy
     prob_state_0 = max(prob_state_0, 0) # make sure no negative probability
-    
+
     return [prob_state_0, prob_state_1, prob_state_2, 0, 0] # return initial occupancy distribution
 
 
@@ -71,9 +70,14 @@ def perform_action(state, action, age):
     # get the next states and probabilities
     next_states = list(P[age][state][action].keys())
     probabilities = np.array(list(P[age][state][action].values()), dtype=float)
-
+    
     probabilities = np.maximum(probabilities, 0) # make sure no negative probabilities
-     
+
+    nan_mask = np.isnan(probabilities) # Check for NaN values
+
+    # Replace NaN values with 0
+    probabilities[nan_mask] = 0
+
     probabilities /= probabilities.sum() # normalize probabilities to sum up to 1
 
     next_state = np.random.choice(next_states, p = probabilities) # choose next state
@@ -85,12 +89,13 @@ def perform_action(state, action, age):
 
     elif next_state in list(range(1, 21)):
         reward = float(R[age]["Early"][action])
-
+    
     else:
         reward = float(R[age]["Advanced"][action])
-
+    
     # return reward and next state
     return next_state, reward
+
 
 # Function to test the policy
 def test_policy(policy, num_episodes = 1000, max_prob_state_1 = 0.2, max_prob_state_2 = 0.3):
@@ -98,35 +103,35 @@ def test_policy(policy, num_episodes = 1000, max_prob_state_1 = 0.2, max_prob_st
     results = []
     
     for _ in range(num_episodes):
-        
+
         # initialize episodes for testing
-        state = int((1 - initialize_occupancy_distribution(max_prob_state_1, max_prob_state_2)[0]) * 100) # get state
-
+        state = round((1 - initialize_occupancy_distribution(max_prob_state_1, max_prob_state_2)[0]) * 100, 1) # get state
+        
         total_reward = 0
-
+        
         for age in range(40, 100):
             
             if state not in policy[age]:
-                print(f"No policy for state {state} at time {age}. Defaulting to 'AM'.")
+                print(f"No policy for state {state} at time {t}. Defaulting to 'AM'.")
                 action = "AM"
             else:
                 action = policy[age][state]
-
+            
             next_state, reward = perform_action(state, action, age) # perform action
             total_reward += reward # calculate total reward
-
+            
             if next_state == 100:  # Absorbing state, exit the loop
                 break
-
+            
             state = next_state
-
+        
         results.append(total_reward)
 
     # return mean rewards and std deviation
     return np.mean(results), np.std(results)
 
 # Load the policy from backward induction data from the CSV file
-bi_policy_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/results/backward_induction_policy.csv")
+bi_policy_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/results/granular_level_results/backward_induction_policy_granular.csv")
 
 # Convert the policy DataFrame into a dictionary
 policy_backward_induction = {}
@@ -139,15 +144,15 @@ for index, row in bi_policy_df.iterrows():
     
     if t not in policy_backward_induction:
         policy_backward_induction[t] = {}
-
+    
     policy_backward_induction[t][state] = optimal_action
 
-ql_policy_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/results/qlearning_policy.csv")
+ql_policy_df = pd.read_csv("/Users/ruchithakor/Downloads/Masters_Docs/MRP/MRP_Optimal_Biopsy_Decision_Making_Breast_Cancer_RL/results/granular_level_results/qlearning_policy_granular.csv")
 
 # Convert the policy DataFrame into a dictionary
 policy_q_learning = {}
-for index, row in ql_policy_df.iterrows():
 
+for index, row in ql_policy_df.iterrows():
     t = row["time_stamp"]
     state = row["state"]
     optimal_action = row["action"]
@@ -159,8 +164,8 @@ for index, row in ql_policy_df.iterrows():
 
 # Test the backward induction policy
 mean_reward_bi, std_reward_bi = test_policy(policy_backward_induction)
-print(f"Backward Induction Policy - Mean Reward: {round(mean_reward_bi, 4)}, Std Reward: {round(std_reward_bi, 4)}")
+print(f"Backward Induction Policy granular - Mean Reward: {round(mean_reward_bi, 4)}, Std Reward: {round(std_reward_bi, 4)}")
 
 # Test the Q-learning policy
 mean_reward_ql, std_reward_ql = test_policy(policy_q_learning)
-print(f"Q-learning Policy - Mean Reward: {round(mean_reward_ql, 4)}, Std Reward: {round(std_reward_ql, 4)}")
+print(f"Q-learning Policy granular - Mean Reward: {round(mean_reward_ql, 4)}, Std Reward: {round(std_reward_ql, 4)}")
